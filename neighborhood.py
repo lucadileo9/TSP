3db5fb999d5c0d5445fb8849a80a3294f76872b4
lucadileo9 <lucadileo70@gmail.com>
2024-10-28 22:29:10 +0100

    feat: swap neighborhood
    I implemented the swap  algorithm to explore new solutions in the neighborhood

--- neighborhood.py
+++ neighborhood.py
@@ -1,43 +1,24 @@
-# Swap: Per ogni nodo del percorso, scambia la posizione del nodo corrente con il successivo.
-
-# Valutazione: Dopo ogni swap, calcola la lunghezza totale del percorso (distanza complessiva).
-
-# Insieme di soluzioni: Aggiungi la nuova soluzione ottenuta dallo swap a un insieme di soluzioni (o vicinato). Alla fine, potrai scegliere la soluzione migliore all'interno di questo insieme.
-
-# Quindi facciamo una funzione che prenda in input un percorso. Per ogni nodo del percorso (tranne l'ultimo), scambia la posizione del nodo corrente con il successivo
-# Poi una sottofunzione che calcoli la lunghezza del percorso. E la inseriamo in un insieme di soluzioni. 
-
-def swap_path(path):
+def swap_neighborhood(path):
     """
-    For each node in the path, swap the position of the current node with the next node.
+    Generate all possible neighbors of a path by swapping two nodes.
     
-    Parameters:
-        path (list): A list of nodes in the graph.
+    Args:
+        path (list): The current path (a list of node indices).
     
     Returns:
-        list: A list of paths obtained by swapping the position of nodes in the path.
+        list: A list of paths, each created by swapping two nodes in the original path.
     """
-    swap_paths = []
-    for i in range(len(path)-1):
-        new_path = path.copy()
-        new_path[i], new_path[i+1] = new_path[i+1], new_path[i]
-        swap_paths.append(new_path)
-    return swap_paths
-
-def path_length(dist, path):
-    """
-    Computes the length of a given path in a graph.
+    neighbors = []
+    n = len(path)
     
-    This function calculates the total length of the given path by summing the distances between consecutive nodes.
+    # Per ogni coppia di nodi (i, j) esegui uno swap
+    for i in range(1, n - 1):        # Evita di scambiare il nodo di partenza e arrivo (0 e ultimo)
+        for j in range(i + 1, n - 1):
+            # Crea una copia del percorso per evitare modifiche sull'originale
+            new_path = path[:]
+            # Esegui lo swap
+            new_path[i], new_path[j] = new_path[j], new_path[i]
+            # Aggiungi il nuovo percorso al vicinato
+            neighbors.append(new_path)
     
-    Parameters:
-        dist (list): A 2D list representing the distances between nodes in the graph.
-        path (list): A list representing the path for which the length is to be calculated.
-    
-    Returns:
-        float: The total length of the path.
-    """
-    total_length = 0
-    for i in range(len(path)-1):
-        total_length += dist[path[i]][path[i+1]]
-    return total_length
+    return neighbors
