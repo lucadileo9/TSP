f2c92842dd1c85d7baa37eab6f56e39b9946178c
lucadileo9 <lucadileo70@gmail.com>
2024-12-01 20:11:55 +0100

    fix: DEBUG bool for checkpath
    Now the CheckPath function prints the problem only if the DEBUG flag is set to true

--- algorithm_metrics.py
+++ algorithm_metrics.py
@@ -6,46 +6,49 @@
 #  - the nodes are part of the graph.
 import timeit
 
-def check_path(points, path):
+def check_path(points, path, DEBUG=False):
     """
     Validates a given path in a graph.
-
-    This function performs several checks to ensure that the provided path is valid:
-    1. The path must be closed (i.e., the first and last nodes must be the same).
-    2. The path must contain all the nodes in the graph.
-    3. The path must only contain nodes that are present in the graph.
-    4. Each node in the path (except for the first and last) must appear only once.
 
     Parameters:
         points (list): A list of nodes in the graph.
         path (list): A list representing the path to be checked.
+        DEBUG (bool): Whether to print debug messages.
 
     Returns:
         bool: True if the path is valid, False otherwise.
     """
-    # check if the path is closed
+    # Check if the path is closed
     if path[0] != path[-1]:
-        print("The path is not closed")
+        if DEBUG:
+            print("The path is not closed")
         return False
-    # check if the path contains all the nodes
+
+    # Check if the path contains all the nodes
     for i in range(len(points)):
         if i not in path:
-            print("The path doesn't contain all the nodes")
-            # Find the missing node
-            for j in range(len(points)):
-                if j not in path:
-                    print(f"Missing node: {j}")
+            if DEBUG:
+                print("The path doesn't contain all the nodes")
+                # Find the missing node
+                for j in range(len(points)):
+                    if j not in path:
+                        print(f"Missing node: {j}")
             return False
-    # check if the path contains only the nodes in the graph
+
+    # Check if the path contains only the nodes in the graph
     for i in path:
         if i >= len(points):
-            print("The path contains nodes not in the graph")
+            if DEBUG:
+                print("The path contains nodes not in the graph")
             return False
-    # check if the path contains the nodes only once, except for the first and the last 
+
+    # Check if the path contains the nodes only once, except for the first and the last
     for i in range(1, len(path) - 1):
         if path.count(path[i]) != 1:
-            print("The path contains nodes more than once")
+            if DEBUG:
+                print("The path contains nodes more than once")
             return False
+
     return True
 
 def path_length(dist, path, print_length=False):
