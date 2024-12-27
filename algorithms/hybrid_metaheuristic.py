b650a949a97fd171a125446df01f4205db2cdcbb
lucadileo9 <lucadileo70@gmail.com>
2024-12-27 10:54:49 +0100

    doc: hybrid metaheuristic

--- algorithms/hybrid_metaheuristic.py
+++ algorithms/hybrid_metaheuristic.py
@@ -1,3 +1,18 @@
+'''
+This module implements a hybrid metaheuristic algorithm combining Iterated Local Search (ILS) and Simulated Annealing (SA) 
+to solve the Traveling Salesman Problem (TSP). The main function `ils_sa_tsp` reads a TSP instance from a TSPLIB file, 
+generates an initial solution, and iteratively improves it using perturbation and simulated annealing.
+Functions:
+    ils_sa_tsp(file_path, iterations, DEBUG=False):
+Usage:
+    To execute this module, run it as a script. It will test the `ils_sa_tsp` function using a sample TSP instance file.
+    Example:
+        python hybrid_metaheuristic.py
+    This will read the TSP instance from "TSP/data/new_instances/a280.tsp", run the ILS-SA algorithm for 10 iterations, 
+    and print the best solution and its cost.à
+    You can change the file path and number of iterations by modifying the arguments of the `ils_sa_tsp` function.
+'''
+
 from .perturbation import *
 from .metaheuristic_algorithms import simulated_annealing
 from tqdm import tqdm
@@ -5,9 +20,17 @@ from tqdm import tqdm
 from ..utils.algorithm_metrics import path_length
 from ..utils.path_utils import generate_random_path, nearest_neighbor_second
 from ..utils.tsp_utils import readTSPLIB
-# In questo file costruiremo un'metaeuristica per risolvere il problema del commesso viaggiatore (TSP) su un'istanza TSPLIB.
 
 def ils_sa_tsp(file_path, iterations, DEBUG=False):
+    """
+    Perform Iterated Local Search (ILS) combined with Simulated Annealing (SA) to solve the Traveling Salesman Problem (TSP).
+    Args:
+        file_path (str): Path to the TSPLIB file containing the TSP instance.
+        iterations (int): Number of iterations for the ILS algorithm.
+        DEBUG (bool, optional): If True, print debug information. Default is False.
+    Returns:
+        tuple: A tuple containing the best solution found and its path length.
+    """
     n, points, dist = readTSPLIB(file_path)
 
     if n > 2000:
